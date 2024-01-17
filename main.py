19247f96b051b771181b41987763b0f6d528ed27
youyu <2000601104@cjlu.edu.cn>
2024-01-17 21:53:58 +0800

    新增一个按钮
    更新了一个方法，增加导入单个书籍的多个书摘到纸间书摘的按钮。
    csv 文件格式见书摘文件夹目录，文件名为书名。

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import csv
 import json
+import os
 
 import requests
 from PySide6 import QtWidgets
 import sys
 
-from PySide6.QtWidgets import QHBoxLayout
+from PySide6.QtWidgets import QHBoxLayout, QFileDialog
 from qt_material import apply_stylesheet
 
 
@@ -59,6 +61,17 @@ class MyWidget(QtWidgets.QWidget):
         lay_right.addRow(self.submit_btn)
         # self.layout.addWidget(line_edit)
 
+        # 添加一些其他的功能
+        self.OQWidget = QtWidgets.QWidget()
+        self.layout.addWidget(self.OQWidget)
+        lay_other = QtWidgets.QFormLayout()
+        self.OQWidget.setLayout(lay_other)
+
+        self.file_to_merpyzf_btn = QtWidgets.QPushButton("将文件导入到纸间书摘")
+        self.file_to_merpyzf_btn.clicked.connect(self.file_to_merpyzf_btn_clicked)
+
+        lay_other.addRow(self.file_to_merpyzf_btn)
+
         self.setLayout(self.layout)
 
     def button_clicked(self):
@@ -83,7 +96,6 @@ class MyWidget(QtWidgets.QWidget):
             return
 
         def submit_note(self):
-
             # 构造数据
             data = {
                 "title": title,  # 书名：必填
@@ -95,18 +107,17 @@ class MyWidget(QtWidgets.QWidget):
                 "isbn": isbn,  # ISBN：选填
                 "type": 1,  # 书籍类型，必填。可取值：0：纸质书；1：电子书
                 "locationUnit": 1,  # 书籍页码类型，必填。可取值：0：进度；1：位置；2：页码
-                "entries": [{
-                    "text": text,
-                    "note": note
-                }]
+                "entries": [{"text": text, "note": note}],
             }
 
             # 发送请求
-            url = 'http://' + ip + ':8080/send'  # 构造请求 URL
+            url = "http://" + ip + ":8080/send"  # 构造请求 URL
 
-            headers = {'ContentType': 'application/json'}  # 设置请求头
+            headers = {"ContentType": "application/json"}  # 设置请求头
 
-            response = requests.post(url, data=json.dumps(data), headers=headers)  # 发送 POST 请求
+            response = requests.post(
+                url, data=json.dumps(data), headers=headers
+            )  # 发送 POST 请求
 
             if response.status_code == 200:  # 判断返回状态码
                 print("保存成功！")
@@ -119,11 +130,87 @@ class MyWidget(QtWidgets.QWidget):
 
         submit_note(self)
 
+    def file_to_merpyzf_btn_clicked(self):
+        ip = self.ip_edit.text()  # 获取 ip 输入
+        if ip == "":
+            QtWidgets.QMessageBox.information(self, "提示", "时刻需要 ip 呀！")
+            return
 
-if __name__ == '__main__':
+        def file_csv_to_merpyzf(file_name):
+            """
+            传入一个 csv 文件，将 csv 名作为书籍信息进行导入
+            :return:
+            """
+            read_info = []
+            # 从 CSV 文件中读取数据
+            with open(file_name, "r", encoding="utf-8") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    read_info.append(row)  # 打印每一行数据
+            return read_info
+
+        def post_to_merpyzf(data, name):
+            for line in data:
+                print(line)
+                note_data = {
+                    "title": name,  # 书名：必填
+                    "cover": "https:#img2.doubanio.com/view/subject/l/public/s29707472.jpg",  # 书籍封面：选填
+                    "author": "author",  # 作者：选填
+                    "translator": "translator",  # 译者：选填
+                    "publisher": "publisher",  # 出版社：选填
+                    "publishDate": 1519833600,  # 出版日期：单位秒，选填
+                    # "isbn": "978752175575",  # ISBN：选填
+                    "type": 1,  # 书籍类型，必填。可取值：0：纸质书；1：电子书
+                    "locationUnit": 0,  # 书籍页码类型，必填。可取值：0：进度；1：位置；2：页码
+                    "entries": [
+                        {
+                            "text": line["原文摘录"],
+                            "note": line["想法"],
+                            "chapter": line["章节名"],
+                            "page": line["进度"],
+                            "timestamp": line["笔记创建的时间"],
+                        }
+                    ],
+                }
+                print(json.dumps(note_data))
+                url = f"http://{ip}:8080/send"
+                headers = {"ContentType": "application/json"}  # 设置请求头
+                response = requests.post(
+                    url,
+                    data=json.dumps(note_data),
+                    headers=headers,
+                )  # 发送 POST 请求
+
+                if response.status_code == 200:
+                    QtWidgets.QMessageBox.information(self, "提示", "保存成功！")
+                else:
+                    QtWidgets.QMessageBox.information(
+                        self, "提示", "保存失败，向开发者反馈一下吧。" "邮箱：youyu273@foxmail.com"
+                    )
+
+        file_path, _ = QFileDialog.getOpenFileName(
+            None, "选择文件", "", "All Files (*.csv)"
+        )
+        print(file_path)
+        data = file_csv_to_merpyzf(file_path)
+        book_name = os.path.basename(file_path)
+        print(book_name)
+        book_name = book_name[:-4]
+
+        post_to_merpyzf(data, book_name)
+
+
+class csv_to_merpyzf:
+    def __init__(self):
+        self.csv_file = ""
+        with open("merpyzf.csv", "r", encoding="utf-8") as f:
+            info = f.readlines()
+
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     # 设置样式表
-    apply_stylesheet(app, theme='dark_teal.xml')
+    apply_stylesheet(app, theme="dark_teal.xml")
     # 标题
     app.setApplicationName("纸间书摘 PC 书评导入")
 
